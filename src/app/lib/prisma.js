839f6521ce6a43e6d7dc978648808aafca9ed751
Waqas Umar <55554486+WAQASUMAR33@users.noreply.github.com>
2025-05-23 17:41:28 +0500

    first commit

--- src/app/lib/prisma.js
+++ src/app/lib/prisma.js
@@ -1,5 +1,9 @@
-const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
-module.exports = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prisma;
