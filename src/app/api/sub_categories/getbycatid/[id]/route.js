b2858c7639933e93812b8635ab7e075e190eb619
Waqas Umar <55554486+WAQASUMAR33@users.noreply.github.com>
2025-05-26 23:10:19 +0500

    2nd commit

---
+++ src/app/api/sub_categories/getbycatid/[id]/route.js
@@ -0,0 +1,17 @@
+import prisma from '../../../../lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(request, { params }) {
+  try {
+    const { id } = params;
+    const subcategories = await prisma.sub_category.findMany({
+      where: { category_id: parseInt(id) },
+    });
+
+   
+    return NextResponse.json(subcategories, { status: 200 });
+  } catch (error) {
+    console.error('Get category error:', error);
+    return NextResponse.json({ error: 'Failed to fetch category' }, { status: 500 });
+  }
+}
--- src/app/api/sub_categories/getsubcategoryid/route.js
+++ src/app/api/sub_categories/getsubcategoryid/route.js
@@ -4,28 +4,28 @@ import { NextResponse } from 'next/server';
 export async function POST(request) {
   try {
     const data = await request.json();
-    const { category_name } = data;
+    const { sub_category_title } = data;
 
     // Validate category_name
-    if (!category_name) {
+    if (!sub_category_title) {
       return NextResponse.json({ error: 'Category name is required' }, { status: 400 });
     }
    
 
     // Check for existing category (case-insensitive)
-    let category = await prisma.category.findFirst({
+    let subcategory_id = await prisma.sub_category.findFirst({
       where: {
-        category_name: {
-          equals: category_name.trim()
+        sub_category_title: {
+          equals: sub_category_title.trim()
         },
       },
       select: {
-        category_id: true,
+        sub_category_id: true,
       },
     });
 
 
-    return NextResponse.json({ category_id: category.category_id }, { status: 200 });
+    return NextResponse.json({ sub_category_id: subcategory_id.sub_category_id }, { status: 200 });
   } catch (error) {
     console.error('Category lookup/create error:', error);
     return NextResponse.json({ error: 'Failed to process category' }, { status: 500 });
